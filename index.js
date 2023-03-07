d4cd93c1ac4011c2cd3c32cc3fdf3768531af265
Amit Kamat <amitkamat918@gmail.com>
2023-03-07 19:12:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+
+//END SECTION :
+
+const section = document.querySelector("section");
+
+//SCROLL MAGIC :
+
+const controller = new ScrollMagic.Controller();
+
+// SCENES
+const scene = new ScrollMagic.Scene({
+  duration: 9500,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setPin(intro)
+  .addTo(controller);
+
+// Video Animation
+
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollpos = e.scrollPos / 1000; // divided by 1000 bcoz we want in seconds
+  console.log(e);
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 41.7);
